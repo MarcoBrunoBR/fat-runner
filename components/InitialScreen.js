3e20ca12591f8db659c0d3109eb8569e890caa98
Artur Diniz Adam <artur.daz@gmail.com>
2017-07-29 19:42:35 -0300

    Offline game screen init

--- components/InitialScreen.js
+++ components/InitialScreen.js
@@ -9,17 +9,21 @@
     const render = (props) => {
         const $component = new DOMComponent()
         $component.html(`
-            <button id="play">
-                Jogar com alguém
+            <button id="startOnline">
+                Jogar com alguém do outro lado do mundo
+            </button>
+            <button id="startOffline">
+                Jogar com alguém do seu lado
             </button>
         `)
-        $component.on("click", "#play", handleMultiPlayerStart)
+        $component.on("click", "#startOnline", handleOnlinePlayerStart)
+        $component.on("click", "#startOffline", handleOfflinePlayerStart)
         return $component.getElement()
     }
 
     const server = new Server("https://fatrunner-server.herokuapp.com")
 
-    const handleMultiPlayerStart = (event) => {
+    const handleOnlinePlayerStart = (event) => {
         const serverConnectionPromise = server.connect()
         serverConnectionPromise
             .then(connection => connection.findPlayer())
@@ -31,4 +35,8 @@
             })
     }
 
-})(window, Objectz.compose, Component, DOMComponent, Server, Game)
+    const handleOfflinePlayerStart = (event) => {
+        Screen.render(OfflineGameScreen)
+    }
+
+})(window, Objectz.compose, Component, DOMComponent, Server, OfflineGameScreen)
