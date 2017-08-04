2149a43b22b96d4b3e99d1add47515f319106e19
Artur Diniz Adam <artur.daz@gmail.com>
2017-08-04 14:16:14 -0300

    Deleting useless code

--- components/LoadingRemoteMatchScreen.js
+++ components/LoadingRemoteMatchScreen.js
@@ -13,12 +13,6 @@
                     console.error(error)
                     server.disconnect()
                 })
-        //quando o ser vivo clicar aqui me avisa
-        match.click()
-
-        //quando o o ser vivo do outro lado clicar eu te aviso
-        match.on("otherPlayerClicked", function(){
-
-        })
+                
     }
 })(window, Server)
